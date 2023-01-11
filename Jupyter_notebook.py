c0fcdee60027b13fc62e567569ed5cf2a7b4c4e1
kptsilva21 <76059753+kptsilva21@users.noreply.github.com>
2023-01-11 13:49:35 -0500

    Add files via upload

---
+++ Jupyter_notebook.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Kalinga Pavan Silva**
+# <br>Data Scientist
+
+# *I am interested in data science because I like to work with numbers and make predictions about certain data.*
+
+# ### The code will print 1+1
+
+# In[1]:
+
+
+print(1+1)
+
+
+# ***
+# * Ant
+# * Goat
+# * Butterfly
+# 
+# ***
+# 1. Cat
+# 2. Dog
+# 3. Pig
+
+# In[ ]:
+
+
+
+
